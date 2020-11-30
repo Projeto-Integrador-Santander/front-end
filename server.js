df830aaaca39c583907d7da742fa38cf337c88c6
gisellykds <gisellykathellynsilva@gmail.com>
2020-11-30 11:10:40 -0300

    Teste deploy configuration

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://educanjos-v1.herokuapp.com')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', 'https://educanjos-v1.herokuapp.com/*')
+    next()
+})
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
